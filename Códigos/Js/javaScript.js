106375142d8a30d914859d3f1014841677a8a354
Matheus Henrique <120841905+matheushnunes@users.noreply.github.com>
2024-05-28 14:10:56 -0300

    Melhorias na responsividade.

--- Códigos/Js/javaScript.js
+++ Códigos/Js/javaScript.js
@@ -1,23 +1,16 @@
-/*
-let fundo = document.querySelector("#img_fundo")
-let divFundo = document.querySelector("#fundo")
-let mais = 20
-let menos = 20
-window.addEventListener('resize',(e)=>{
-    let width = divFundo.offsetWidth
-    let divFundoMargin = divFundo.style.marginTop.match(/\d+/ig)
-    mais++
-    menos--
-    if(divFundoMargin != null){
-        divFundoMargin = parseInt(divFundoMargin[0])
-    }else{
-    }
-    console.log(width)
-    console.log(divFundoMargin)
-    if (width >= 524 & divFundo <= 190) {
-        divFundo.style.marginTop = menos+"px"
-    } else if (width <= 523) {
-        divFundo.style.marginTop = mais+"px"
-    }
-})
-*/
+// let imgFundo = document.querySelector("#img_fundo")
+// let tela = document.querySelector("html")
+// function marginFundo() {
+//     console.log(tela.offsetWidth)
+//     if (tela.offsetWidth < 1156) {
+//         imgFundo.style.marginTop = "80px"
+//         console.log(imgFundo.style.marginTop)
+//     }else {
+//         imgFundo.style.marginTop = '0'
+//     }
+// }
+// window.addEventListener('resize',(e)=>{
+//     marginFundo()
+// })
+// window.onload = marginFundo()
+
