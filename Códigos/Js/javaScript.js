d0da3ef2d7678aac5547d372ec874cbcd5fd1d9f
Caio cesar <caiocesar3635@gmail.com>
2024-06-03 20:49:18 -0300

    codigo de tela de cadastro em popup

--- Códigos/Js/javaScript.js
+++ Códigos/Js/javaScript.js
@@ -1,16 +1,34 @@
-// let imgFundo = document.querySelector("#img_fundo")
-// let tela = document.querySelector("html")
-// function marginFundo() {
-//     console.log(tela.offsetWidth)
-//     if (tela.offsetWidth < 1156) {
-//         imgFundo.style.marginTop = "80px"
-//         console.log(imgFundo.style.marginTop)
-//     }else {
-//         imgFundo.style.marginTop = '0'
-//     }
-// }
-// window.addEventListener('resize',(e)=>{
-//     marginFundo()
-// })
-// window.onload = marginFundo()
+ let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Rolando para baixo
+        header.classList.add('header-hidden');
+    } else {
+        // Rolando para cima
+        header.classList.remove('header-hidden');
+    }
+
+    lastScrollTop = scrollTop;
+}); 
+
+let imgFundo = document.querySelector("#img_fundo")
+ let tela = document.querySelector("html")
+ function marginFundo() {
+     console.log(tela.offsetWidth)
+     if (tela.offsetWidth < 1156) {
+         imgFundo.style.marginTop = "80px"
+         console.log(imgFundo.style.marginTop)
+     }else {
+         imgFundo.style.marginTop = '0'
+     }
+ }
+ window.addEventListener('resize',(e)=>{
+     marginFundo()
+ })
+ window.onload = marginFundo()
+
 
